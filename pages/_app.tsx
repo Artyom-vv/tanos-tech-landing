fc6f59e8b758ecc5f9966dee959060426c799e77
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-01-29 01:28:43 +0300

    Интеграция tetrisly. Изучение библиотеки

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import dynamic from 'next/dynamic';
+
+const TetrislyProvider = dynamic(() => import('@virtuslab/tetrisly-react').then(mod => mod.TetrislyProvider), {ssr: false});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <TetrislyProvider>
+          <Component {...pageProps} />
+      </TetrislyProvider>
+  )
 }
