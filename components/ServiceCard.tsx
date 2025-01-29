6c4a7877b66d698d41c0f31869ba9f2c2c190234
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-01-29 22:25:08 +0300

    Доработка блока services

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,5 +1,5 @@
 import {tet} from "@virtuslab/tetrisly-react";
-import {HTMLAttributes, ReactNode} from "react";
+import {HTMLAttributes, ReactNode, useState} from "react";
 import cn from 'classnames'
 
 interface ServiceCardProps {
@@ -8,29 +8,44 @@ interface ServiceCardProps {
     children: ReactNode
 }
 
+const overlayLinearGradient = "linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.00251556) 11.79%, rgba(0, 0, 0, 0.00972445) 21.38%, rgba(0, 0, 0, 0.02112) 29.12%, rgba(0, 0, 0, 0.0361956) 35.34%, rgba(0, 0, 0, 0.0544444) 40.37%, rgba(0, 0, 0, 0.07536) 44.56%, rgba(0, 0, 0, 0.0984356) 48.24%, rgba(0, 0, 0, 0.123164) 51.76%, rgba(0, 0, 0, 0.14904) 55.44%, rgba(0, 0, 0, 0.175556) 59.63%, rgba(0, 0, 0, 0.202204) 64.66%, rgba(0, 0, 0, 0.22848) 70.88%, rgba(0, 0, 0, 0.253876) 78.62%, rgba(0, 0, 0, 0.277884) 88.21%, rgba(0, 0, 0, 0.3) 100%);"
+
 export function ServiceCard({src, title, children, ...props}: ServiceCardProps & HTMLAttributes<HTMLDivElement>) {
+    const [flipped, setFlipped] = useState(false);
+
     return (
         <tet.div
             {...props}
             className={cn("w-[320px] h-[320px]", props.className)}
-            background={`url("${src}")`}
+            background={flipped ? "" : `url("${src}")`}
             borderRadius="$border-radius-large"
-            padding="$space-component-padding-2xLarge"
+            onMouseEnter={() => setFlipped(true)}
+            onMouseLeave={() => setFlipped(false)}
         >
-            <tet.h3
-                text="$typo-header-3xLarge"
-                color="$color-content-primary-inverted"
+            <tet.div
+                backgroundColor={flipped ? "$color-background-default" : "transparent"}
+                borderRadius="$border-radius-large"
+                className={cn("border-[1px] border-transparent h-full transition", {
+                    "!border-[#EAEDF0]": flipped
+                })}
+                padding="$space-component-padding-2xLarge"
             >
-                {title}
-            </tet.h3>
-            <tet.p
-                text="$typo-body-large"
-                color="$color-content-secondary"
-            >
-                {children}
-            </tet.p>
+                <tet.h3
+                    text="$typo-header-3xLarge"
+                    color={flipped ? "$color-content-primary" : "$color-content-primary-inverted"}
+                    className="transition"
+                >
+                    {title}
+                </tet.h3>
+                <tet.p
+                    text="$typo-body-large"
+                    color="$color-content-secondary"
+                    className="mt-2 transition"
+                    opacity={flipped ? 1 : 0}
+                >
+                    {children}
+                </tet.p>
+            </tet.div>
         </tet.div>
-    // borderColor="$color-border-default"
-    // borderWidth="1px"
     )
 }
--- screens/Services.tsx
+++ screens/Services.tsx
@@ -1,6 +1,6 @@
 import cn from 'classnames'
 import {sectionPaddings} from "@/components/tailwind-classnames";
-import {tet} from '@virtuslab/tetrisly-react';
+import {Button, tet} from '@virtuslab/tetrisly-react';
 import React from "react";
 import {ServiceCard} from "@/components/ServiceCard";
 
@@ -8,6 +8,7 @@ export function Services() {
     return (
         <tet.section
             backgroundColor="$color-background-neutral-subtle"
+            className="rounded-[32px]"
         >
             <div className={cn('container grid grid-cols-2 gap-4', sectionPaddings)}>
                 <div className="col-span-1 grid grid-cols-2 grid-rows-3 gap-4">
@@ -27,25 +28,46 @@ export function Services() {
                         </tet.p>
                     </div>
                     <ServiceCard src="/images/web-apps.png" title="Сайты и приложения" className="col-start-2">
-                        text
+                        Lorem ipsum odor amet, consectetuer adipiscing elit.
                     </ServiceCard>
                 </div>
                 <div className="col-span-1 grid grid-cols-2 grid-rows-3 gap-4">
                     <ServiceCard src="/images/web-services.png" title="Веб-сервисы" className="col-span-1">
-                        test
+                        Создаем масштабируемые решения, которые обеспечивают рост вашего бизнеса и адаптацию  к изменениям рынка
                     </ServiceCard>
                     <ServiceCard src="/images/erp-crm.png" title="ERP и CRM-системы" className="col-span-1">
-                        test
+                        Lorem ipsum odor amet, consectetuer adipiscing elit.
                     </ServiceCard>
                     <ServiceCard src="/images/it-consulting.png" title="ИТ-консалтинг и поддержка" className="col-span-1">
-                        test
+                        Lorem ipsum odor amet, consectetuer adipiscing elit.
                     </ServiceCard>
                     <ServiceCard src="/images/it-algorithmics.png" title="ИИ-алгоритмы" className="row-start-3 col-span-1">
-                        test
+                        Lorem ipsum odor amet, consectetuer adipiscing elit.
                     </ServiceCard>
-                    {/*<ServiceCard src="" title={} className="row-start-3 col-span-1">*/}
-                    {/*    test*/}
-                    {/*</ServiceCard>*/}
+                    <Button
+                        className={cn("row-start-3 col-span-1")}
+                        custom={{
+                            ghost: {
+                                appearance: {
+                                    inverted: {
+                                        boxShadow: '',
+                                        bg: {
+                                            _: '$color-action-primary-normal',
+                                            hover: '$color-action-primary-hover',
+                                            active: '$color-action-primary-active',
+                                        },
+                                        h: '320px',
+                                        w: '320px',
+                                        borderRadius: '$border-radius-large',
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent:"center"
+                                    }
+                                },
+                            }
+                        }}
+                        variant="ghost" appearance="inverted" size="large">Заказать проект</Button>
+
                 </div>
             </div>
 
