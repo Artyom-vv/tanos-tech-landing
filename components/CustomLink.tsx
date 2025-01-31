164c1850f4c57e0ecbb55f7ac75fa6ff2f85c29f
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-02-01 00:08:24 +0300

    fff

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -3,10 +3,9 @@ import React, {ReactNode} from "react";
 
 interface HeaderLinkProps {
     children: ReactNode;
-    to?: string;
 }
 
-export const CustomLink = ({children, to}: HeaderLinkProps) => (
+export const CustomLink = ({children}: HeaderLinkProps) => (
     <tet.a className="transition hover:text-gray-500 py-1 px-3 cursor-pointer whitespace-nowrap select-none"
            color="$color-action-neutral-normal"
            text="$typo-body-medium">{children}</tet.a>
