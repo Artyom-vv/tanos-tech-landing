20e587a5aa97ff0fd33b5188c1f3f0351bac8982
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-02-01 00:00:24 +0300

    Правки

--- screens/Services.tsx
+++ screens/Services.tsx
@@ -11,36 +11,37 @@ export function Services() {
             className="rounded-[32px]"
         >
             <div className={cn('container grid grid-cols-2 gap-4', sectionPaddings)}>
-                <div className="col-span-1 grid grid-cols-2 grid-rows-3 gap-4">
-                    <div className="col-span-2">
-                        <tet.h2
-                            text="$typo-header-3xLarge"
-                            color="$color-content-primary"
-                            className="mb-2"
-                        >
-                            Услуги
-                        </tet.h2>
-                        <tet.p
-                            text="$typo-body-large"
-                            color="$color-content-secondary"
-                        >
-                            Каждый проект - это новая возможность
-                        </tet.p>
-                    </div>
-                    <ServiceCard src="/images/web-apps.png" title="Сайты и приложения" className="col-start-2">
-                        Lorem ipsum odor amet, consectetuer adipiscing elit.
-                    </ServiceCard>
+                <div className="col-span-1">
+                    <tet.h2
+                        text="$typo-header-3xLarge"
+                        color="$color-content-primary"
+                        className="mb-2"
+                    >
+                        Услуги
+                    </tet.h2>
+                    <tet.p
+                        text="$typo-body-large"
+                        color="$color-content-secondary"
+                    >
+                        Каждый проект - это новая возможность
+                    </tet.p>
                 </div>
                 <div className="col-span-1 grid grid-cols-2 grid-rows-3 gap-4">
                     <ServiceCard src="/images/web-services.png" title="Веб-сервисы" className="col-span-1">
-                        Создаем масштабируемые решения, которые обеспечивают рост вашего бизнеса и адаптацию  к изменениям рынка
+                        Создаем масштабируемые решения, которые обеспечивают рост вашего бизнеса и адаптацию к
+                        изменениям рынка
                     </ServiceCard>
                     <ServiceCard src="/images/erp-crm.png" title="ERP и CRM-системы" className="col-span-1">
                         Lorem ipsum odor amet, consectetuer adipiscing elit.
                     </ServiceCard>
-                    <ServiceCard src="/images/it-consulting.png" title="ИТ-консалтинг и поддержка" className="col-span-1">
-                        Lorem ipsum odor amet, consectetuer adipiscing elit.
-                    </ServiceCard>
+                    <div className="row-start-2 col-span-2 flex gap-4 -translate-x-[336px]">
+                        <ServiceCard src="/images/web-apps.png" title="Сайты и приложения" className="col-start-2">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit.
+                        </ServiceCard>
+                        <ServiceCard src="/images/it-consulting.png" title="ИТ-консалтинг и поддержка" className="col-span-1">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit.
+                        </ServiceCard>
+                    </div>
                     <ServiceCard src="/images/it-algorithmics.png" title="ИИ-алгоритмы" className="row-start-3 col-span-1">
                         Lorem ipsum odor amet, consectetuer adipiscing elit.
                     </ServiceCard>
